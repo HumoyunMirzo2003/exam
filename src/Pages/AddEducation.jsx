d78a6e59dd0d8fe43210e6419e5a2a403ea90326
HumoyunMirzo2003 <nabiyevxumoyunmirzo@gmail.com>
2023-02-16 15:34:58 +0500

    2

--- src/Pages/AddEducation.jsx
+++ src/Pages/AddEducation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import  {useState } from 'react'
 import {Link,useNavigate} from 'react-router-dom'
 import { toast } from 'react-toastify'
-import ProfileHeader from '../components/ProfileHeader'
+import ProfileHeader from '../Components/ProfileHeader'
 import axios from 'axios'
 
 const AddEducation = () => {
